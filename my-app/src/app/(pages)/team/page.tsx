901fbce58014171d7959fa2bedb87cf789fb2969
Dinesh Korukonda <dineshkorukonda05@gmail.com>
2024-10-30 12:42:32 +0530

    modifitied team page

--- my-app/src/app/(pages)/team/page.tsx
+++ my-app/src/app/(pages)/team/page.tsx
@@ -1,7 +1,8 @@
+"use client";
 import './Team.css';
 import React from 'react';
-import Pic from "../../assets/Pic.png";
-import { FaInstagram } from 'react-icons/fa';
+import Pic from "../../assets/TeamPerson.webp";
+import { FaInstagram, FaTelegram } from 'react-icons/fa';
 import Footer from '@/app/components/Footer/Footer';
 import Image from 'next/image';
 import { teamMembers } from '@/app/data/team';
@@ -11,66 +12,80 @@ const Team = () => {
   return (
     <div className="TeamPage-container">
       <div className="TeamPage-container-in">
-        <div className="Team-one">
-          <div className="Team-one-in">
-              <h1>Citramela Team</h1>
-          </div>
+        <div className="TeamPage-container-in-heading">
+          <h1>Citramela Team</h1>
+          <p>Meet the team that makes Citramela possible</p>
         </div>
-        <div className="Team-two">
-          <div className="Team-two-in">
-            <div className="Team-two-in-one">
-              <div className="Team-two-in-one-in">
-              <Image src={Pic} alt="Chief" width={250} height={250} />
-                <div className="description">
-                    <p>Name</p>
-                    <p>Chief</p>
-                    <p>Contact : <FaInstagram/> </p>
-                  </div>
-              </div>              
-            </div>
-            <div className="Team-two-in-two">
-              <div className="Team-two-in-two-in">
-                <div className="Team-two-in-two-in-one">
+        <div className="TeamPage-container-in-content">
+          <div className="TeamPage-container-in-content-in">
+            <div className="TeamPage-content-in-one">
+              <div className="TeamPage-Profile">
                 <Image src={Pic} alt="Chief" width={250} height={250} />
-                  <div className="description">
-                    <p>Name</p>
-                    <p>Chief</p>
-                    <p>Contact : <FaInstagram/> </p>
-                  </div>
+                <div className="description">
+                  <p>Name</p>
+                  <p>Chief</p>
                 </div>
-                <div className="Team-two-in-two-in-one">
-                  <Image src={Pic} alt="Chief" width={250} height={250} />
-                  <div className="description">
-                    <p>Name</p>
-                    <p>Chief</p>
-                    <p>Contact : <FaInstagram/> </p>
+                <div className="social-icons">
+                    <Link href="#" legacyBehavior>
+                      <a className='media'><FaInstagram /></a>
+                    </Link>
+                    <Link href="#" legacyBehavior>
+                      <a className='media'><FaTelegram /></a>
+                    </Link>
                   </div>
-                </div>
               </div>
             </div>
-            <div className="Team-two-in-three">
-              <div className="Team-two-in-three-in">
-                {
-                  teamMembers.map((member,index)=>(
-                    <div key={index} className="Team-two-in-three-in-one">
-                      <Image src={member.image} alt={member.designation} width={250} height={250}/>
-                      <div className="description">
-                        <p>{member.name}</p>
-                        <p>{member.designation}</p>
-                        <p className='social'>Contact: <Link href={member.instagram} legacyBehavior ><a className='media'><FaInstagram fontSize={20}/></a></Link></p>
-                      </div>
-                    </div>
-                    
+          </div>
+          <div className="TeamPage-content-in-two">
+            <div className="TeamPage-content-in-two-in">
+              <div className="TeamPage-Profile">
+              {[1, 2].map((item) => (
+                <div key={`static-member-${item}`} className="TeamPage-content-in-two-in-one">
+                  <Image src={Pic} alt="Chief" width={200} height={200} />
+                  <div className="description">
+                    <p>Name</p>
+                    <p>Chief</p>
+                  </div>
+                  <div className="social-icons">
+                      <Link href="#" legacyBehavior>
+                        <a className='media'><FaInstagram /></a>
+                      </Link>
+                      <Link href="#" legacyBehavior>
+                        <a className='media'><FaTelegram /></a>
+                      </Link>
+                  </div>
+                </div>
                 ))}
-              </div>
+              </div>  
+            </div>
+          </div>
+          <div className="TeamPage-content-in-three">
+            <div className="TeamPage-content-in-three-in">
+                <div className="TeamPage-Profile">
+                {teamMembers.map((member) => (
+                <div key={member.id} className="TeamPage-content-in-three-in-one">
+                  <Image src={member.image} alt={member.name} width={200} height={200} />
+                  <div className="description">
+                    <p>{member.name}</p>
+                    <p>{member.designation}</p>
+                  </div>
+                  <div className="social-icons">
+                      <Link href={member.instagram} legacyBehavior>
+                        <a className='media'><FaInstagram /></a>
+                      </Link>
+                      <Link href={member.telegram || "#"} legacyBehavior>
+                        <a className='media'><FaTelegram /></a>
+                      </Link>
+                    </div>
+                </div>
+              ))}
+                </div>
             </div>
           </div>
         </div>
-        <div className="Team-three">
-          <div className="Team-three-in">
-            <Footer/>
-          </div>
-        </div>
+      </div>
+      <div className="TeamPage-footer">
+        <Footer />
       </div>
     </div>
   );
--- my-app/src/app/data/team.ts
+++ my-app/src/app/data/team.ts
@@ -1,86 +1,59 @@
-export const teamMembers = [
+interface TeamMember {
+  id: number;
+  name: string;
+  image: string;
+  designation: string;
+  instagram: string;
+  telegram: string;
+}
+
+export const teamMembers: TeamMember[] = [
   {
-    name: ' Full Name',
-    image: 'https://firebasestorage.googleapis.com/v0/b/bloggervlo-4255a.appspot.com/o/Pic.png?alt=media&token=89a4911b-e5a6-4522-aacf-ff578f6fc72c',
-    designation: 'Chief',
-    instagram: 'https://instagram.com/johndoe'
+    id: 1,
+    name: "Sarah Johnson",
+    image: "/path/to/image1",
+    designation: "Creative Director",
+    instagram: "https://instagram.com/sarahjcreative",
+    telegram: "https://t.me/sarahjcreative"
   },
   {
-    name: ' Full Name',
-    image: 'https://firebasestorage.googleapis.com/v0/b/bloggervlo-4255a.appspot.com/o/Pic.png?alt=media&token=89a4911b-e5a6-4522-aacf-ff578f6fc72c',
-    designation: 'Chief',
-    instagram: 'https://instagram.com/johndoe'
+    id: 2,
+    name: "Michael Chen",
+    image: "/path/to/image2",
+    designation: "Lead Designer",
+    instagram: "https://instagram.com/mchendesigns",
+    telegram: "https://t.me/mchendesigns"
   },
   {
-    name: ' Full Name',
-    image: 'https://firebasestorage.googleapis.com/v0/b/bloggervlo-4255a.appspot.com/o/Pic.png?alt=media&token=89a4911b-e5a6-4522-aacf-ff578f6fc72c',
-    designation: 'Chief',
-    instagram: 'https://instagram.com/johndoe'
+    id: 3,
+    name: "Emma Rodriguez",
+    image: "/path/to/image3",
+    designation: "UI/UX Designer",
+    instagram: "https://instagram.com/emmadesigns",
+    telegram: "https://t.me/emmadesigns"
   },
   {
-    name: ' Full Name',
-    image: 'https://firebasestorage.googleapis.com/v0/b/bloggervlo-4255a.appspot.com/o/Pic.png?alt=media&token=89a4911b-e5a6-4522-aacf-ff578f6fc72c',
-    designation: 'Chief',
-    instagram: 'https://instagram.com/johndoe'
+    id: 4,
+    name: "David Kim",
+    image: "/path/to/image4",
+    designation: "Visual Artist",
+    instagram: "https://instagram.com/davidkimart",
+    telegram: "https://t.me/davidkimart"
   },
   {
-    name: ' Full Name',
-    image: 'https://firebasestorage.googleapis.com/v0/b/bloggervlo-4255a.appspot.com/o/Pic.png?alt=media&token=89a4911b-e5a6-4522-aacf-ff578f6fc72c',
-    designation: 'Chief',
-    instagram: 'https://instagram.com/johndoe'
+    id: 5,
+    name: "Lisa Patel",
+    image: "/path/to/image5",
+    designation: "Project Manager",
+    instagram: "https://instagram.com/lisapatel",
+    telegram: "https://t.me/lisapatel"
   },
   {
-    name: ' Full Name',
-    image: 'https://firebasestorage.googleapis.com/v0/b/bloggervlo-4255a.appspot.com/o/Pic.png?alt=media&token=89a4911b-e5a6-4522-aacf-ff578f6fc72c',
-    designation: 'Chief',
-    instagram: 'https://instagram.com/johndoe'
-  },
-  {
-    name: ' Full Name',
-    image: 'https://firebasestorage.googleapis.com/v0/b/bloggervlo-4255a.appspot.com/o/Pic.png?alt=media&token=89a4911b-e5a6-4522-aacf-ff578f6fc72c',
-    designation: 'Chief',
-    instagram: 'https://instagram.com/johndoe'
-  },
-  {
-    name: ' Full Name',
-    image: 'https://firebasestorage.googleapis.com/v0/b/bloggervlo-4255a.appspot.com/o/Pic.png?alt=media&token=89a4911b-e5a6-4522-aacf-ff578f6fc72c',
-    designation: 'Chief',
-    instagram: 'https://instagram.com/johndoe'
-  },
-  {
-    name: ' Full Name',
-    image: 'https://firebasestorage.googleapis.com/v0/b/bloggervlo-4255a.appspot.com/o/Pic.png?alt=media&token=89a4911b-e5a6-4522-aacf-ff578f6fc72c',
-    designation: 'Chief',
-    instagram: 'https://instagram.com/johndoe'
-  },
-  {
-    name: ' Full Name',
-    image: 'https://firebasestorage.googleapis.com/v0/b/bloggervlo-4255a.appspot.com/o/Pic.png?alt=media&token=89a4911b-e5a6-4522-aacf-ff578f6fc72c',
-    designation: 'Chief',
-    instagram: 'https://instagram.com/johndoe'
-  },
-  {
-    name: ' Full Name',
-    image: 'https://firebasestorage.googleapis.com/v0/b/bloggervlo-4255a.appspot.com/o/Pic.png?alt=media&token=89a4911b-e5a6-4522-aacf-ff578f6fc72c',
-    designation: 'Chief',
-    instagram: 'https://instagram.com/johndoe'
-  },
-  {
-    name: ' Full Name',
-    image: 'https://firebasestorage.googleapis.com/v0/b/bloggervlo-4255a.appspot.com/o/Pic.png?alt=media&token=89a4911b-e5a6-4522-aacf-ff578f6fc72c',
-    designation: 'Chief',
-    instagram: 'https://instagram.com/johndoe'
-  },
-  {
-    name: ' Full Name',
-    image: 'https://firebasestorage.googleapis.com/v0/b/bloggervlo-4255a.appspot.com/o/Pic.png?alt=media&token=89a4911b-e5a6-4522-aacf-ff578f6fc72c',
-    designation: 'Chief',
-    instagram: 'https://instagram.com/johndoe'
-  },
-  {
-    name: ' Full Name',
-    image: 'https://firebasestorage.googleapis.com/v0/b/bloggervlo-4255a.appspot.com/o/Pic.png?alt=media&token=89a4911b-e5a6-4522-aacf-ff578f6fc72c',
-    designation: 'Chief',
-    instagram: 'https://instagram.com/johndoe'
+    id: 6,
+    name: "Alex Thompson",
+    image: "/path/to/image6",
+    designation: "Content Creator",
+    instagram: "https://instagram.com/alexcreates",
+    telegram: "https://t.me/alexcreates"
   }
 ];
