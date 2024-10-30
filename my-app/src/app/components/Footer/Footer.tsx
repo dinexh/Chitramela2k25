3a1544020226bd6f1e23d016defb68bfde8e2e32
Dinesh Korukonda <dineshkorukonda05@gmail.com>
2024-10-30 10:11:59 +0530

    adjusted footer

--- my-app/src/app/components/Footer/Footer.tsx
+++ my-app/src/app/components/Footer/Footer.tsx
@@ -8,35 +8,28 @@ const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer-in">
-        <div className="footer-in-contact">
+        <div className="footer-in-one">
           <p>Designed and Developed by</p> 
-          <p><Link href="www.amazon.in">Dinesh Korukonda</Link> & </p>
-          <p><Link href="www.amazon.in">Pavan Karthik Garaga</Link> of </p>
+          <p><Link href="www.amazon.in">Dinesh Korukonda</Link> & <Link href="www.amazon.in">Pavan Karthik Garaga</Link> of </p>
           <p><Link href="www.amazon.in"> <span>ZeroOne Code Club</span></Link></p>
           <p>Copyright &copy; 2024 Chitramela. All rights reserved.</p>
         </div>
-        <div className="footer-in-one">
-          <Image src={Logo} alt="Company Logo" width={400} height={100} />
-        </div>
         <div className="footer-in-two">
-          <div className="footer-in-two-in">
+          <Image src={Logo} alt="Company Logo" width={400} height={200} />
+        </div>
+        <div className="footer-in-three">
+          <div className="footer-in-three-in-one">
             <Link href="#" className="footer-social-link" aria-label="Facebook" rel="noopener noreferrer">
               <FaFacebookF />
             </Link>
-            {/* <Link href="#" className="footer-social-link" aria-label="Twitter" rel="noopener noreferrer">
-              <FaTwitter />
-              </Link> */}
             <Link href="#" className="footer-social-link" aria-label="Instagram" rel="noopener noreferrer">
               <FaInstagram />
             </Link>
             <Link href="#" className="footer-social-link" aria-label="YouTube" rel="noopener noreferrer">
               <FaYoutube />
             </Link>
-            {/* <Link href="#" className="footer-social-link" aria-label="LinkedIn" rel="noopener noreferrer">
-              <FaLinkedinIn />
-              </Link> */}
           </div>
-          <div className="contact-in">
+          <div className="footer-in-three-in-two">
               <h2>Contact</h2>
               <p>
                 <Link href="tel:+911234567890" legacyBehavior>
@@ -49,13 +42,8 @@ const Footer = () => {
                 </Link>
               </p>
             </div>
-
-
          </div>
       </div>
-
-      
-      
     </footer>
   );
 }
